d9b66cc546a77200c0636cf0c398a6ea9eb0b8cc
lucas bentes <lucasbentes16@gmail.com>
2022-07-07 15:40:53 -0300

    primairos passos

---
+++ rest-with-spring-boot/src/main/java/br/com/lucasbentes/crud/GreetingController.java
@@ -0,0 +1,10 @@
+package br.com.lucasbentes.crud;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	private static final String template = "Hello,%s!";
+
+}
