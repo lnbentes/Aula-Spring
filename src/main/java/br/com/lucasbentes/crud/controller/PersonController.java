83acf1f86692a6697190c5f86c6ea8779bcb5293
lucas bentes <lucasbentes16@gmail.com>
2022-07-08 17:15:46 -0300

    finalizando o personController

--- src/main/java/br/com/lucasbentes/crud/controller/PersonController.java
+++ src/main/java/br/com/lucasbentes/crud/controller/PersonController.java
@@ -1,28 +1,26 @@
 package br.com.lucasbentes.crud.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import br.com.lucasbentes.crud.model.Person;
 import br.com.lucasbentes.crud.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
+@RequestMapping("/person")
 public class PersonController {
 
 	@Autowired
 	private PersonService service;
 	
-	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
-	public Double sum(
-			@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo
-			) throws Exception{
-		
-		return 1D;
+	@RequestMapping(value = "/{id}",
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) throws Exception{
+		return service.findById(id);
 	}
 
 
