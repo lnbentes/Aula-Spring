2d09464d99e6dfee33c04b76c06d8f8ce107e885
lucas bentes <lucasbentes16@gmail.com>
2022-07-08 16:41:14 -0300

    criando a model

---
+++ src/main/java/br/com/lucasbentes/crud/model/Person.java
@@ -0,0 +1,66 @@
+package br.com.lucasbentes.crud.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String fistName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public Person(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFistName() {
+        return fistName;
+    }
+
+    public void setFistName(String fistName) {
+        this.fistName = fistName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFistName(), person.getFistName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFistName(), getLastName(), getAddress(), getGender());
+    }
+}
